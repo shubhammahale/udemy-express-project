b584371cf69a13708fcb2ff3fcab485752f0fbf7
Shubham Mahale <shubham.mahale2@ibm.com>
2022-04-27 14:34:52 +0530

    change getPlacesByUserId logic

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -59,9 +59,10 @@ const getPlaceById = async (req, res, next) => {
 
 const getPlacesByUserId = async (req, res, next) => {
   const userId = req.params.uid;
-  let places;
+
+  let userWithPlaces;
   try {
-    places = await await Place.find({ creator: userId }).exec();
+    userWithPlaces = await User.findById(userId).populate("places");
   } catch (err) {
     const error = new HttpError(
       "Fetching places failed, please try again later.",
@@ -70,14 +71,16 @@ const getPlacesByUserId = async (req, res, next) => {
     return next(error);
   }
 
-  if (!places || places.length === 0) {
+  if (!userWithPlaces || userWithPlaces.places.length === 0) {
     return next(
       new HttpError("Could not find places for the provided user id.", 404)
     );
   }
 
   res.json({
-    places: places.map((place) => place.toObject({ getters: true })),
+    places: userWithPlaces.places.map((place) =>
+      place.toObject({ getters: true })
+    ),
   });
 };
 
