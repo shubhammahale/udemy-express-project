521b1f326bff7d544624e1d08dee2d93bb1ffb0a
Shubham Mahale <shubham.mahale2@ibm.com>
2022-06-24 00:18:33 +0530

    cors related stuff

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST,PATCH, DELET");
+  next();
+});
+
 app.use("/api/places", placesRoutes);
 app.use("/api/users", usersRoutes);
 
@@ -28,7 +38,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://shubham:0RFW9uopQZSBo9fb@cluster0.bwqcf.mongodb.net/places?retryWrites=true&w=majority"
+    "mongodb+srv://shubham:0RFW9uopQZSBo9fb@cluster0.bwqcf.mongodb.net/mern?retryWrites=true&w=majority"
   )
   .then(() => {
     app.listen(5000);
